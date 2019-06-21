5c82f412ba11ba02ffe7b6bfd068ef5972601aed
Avinash711 <48080428+Avinash711@users.noreply.github.com>
2019-06-21 14:48:17 +0530

    weightconversion page

---
+++ myScript.js
@@ -0,0 +1,5 @@
+ var kg=prompt("Weight in Kgs?")
+ var lbs=kg/0.454
+ alert("That is: "+lbs+" Pounds.")
+ console.log("Conversion completed")
+ alert("Conversion completed: click Homepage Button to Return to HomePage")
